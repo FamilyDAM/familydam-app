647e1559e2ee959bba723497c5ad94bba134f4d5
Mike Nimer <mikenimer@yahoo.com>
2016-06-28 00:39:37 -0500

    update configs

--- _nas-bundles/drobo/service.sh
+++ _nas-bundles/drobo/service.sh
@@ -11,30 +11,40 @@ version="0.1.0"
 description="A Digital Asset Manager for Families"
 framework_version="2.1"
 depends="java8"
-webui=""
+webui=":9000/index.html"
 
 prog_dir="$(dirname "$(realpath "${0}")")"
 java_tmp_dir="${prog_dir}/tmp"
 tmp_dir="/tmp/DroboApps/${name}"
 pidfile="${tmp_dir}/pid.txt"
-logfile="${tmp_dir}/log.txt"
-statusfile="${tmp_dir}/status.txt"
-errorfile="${tmp_dir}/error.txt"
+
+logfile="${tmp_dir}/familydam-log.txt"
+statusfile="${tmp_dir}/familydam-status.txt"
+errorfile="${tmp_dir}/familydam-error.txt"
+
 daemon="${DROBOAPPS_DIR}/java8/bin/java"
-jarpath="${prog_dir}/FamilyDAM.jar"
-java_opts=-Djcr.repo.home=./familydam-repo
+jarfile="FamilyDAM-${version}.jar"
+repository="/mnt/DroboFS/Shares/FamilyDAM/local"
+java_opts=-Xmx1024m
+sling_opts="-p 9000 -f ${logfile} -l DEBUG  -c ${repository}/familydam-home -Drepository.home=${repository}/familydam-home/repository"
+
+
 
 start()
 {
-  # if this file doesn't exist, client connections get some ugly warnings.
-  touch /var/log/lastlog
+    # if this file doesn't exist, client connections get some ugly warnings.
+    touch /var/log/lastlog
 
-  if [ ! -d "${java_tmp_dir}" ]; then
-    mkdir -p "${java_tmp_dir}"
-  fi
+    if [ ! -d "${java_tmp_dir}" ]; then
+      mkdir -p "${java_tmp_dir}"
+    fi
+
+    #copy jar to repository local
+    #cp ${jarfile} ${repository}
 
 
-    setsid "${daemon}" ${java_opts} -jar ${jarpath} &
+    echo "${daemon}" ${java_opts} -jar ${prog_dir}/${jarfile} ${sling_opts}
+    setsid "${daemon}" ${java_opts} -jar ${prog_dir}/${jarfile} ${sling_opts} &
     if [ $! -gt 0 ]; then
         local pid=$!
         echo "${pid}" > "${pidfile}"
